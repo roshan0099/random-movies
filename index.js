8d49a07d960e5a707b98d66310e8888d5eafbad2
Roshan <67196874+roshan0099@users.noreply.github.com>
2021-01-14 01:18:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(express.static(__dirname+'/public'))
-// app.use(favicon(__dirname+'/public'+'/favicon.ico'))
+app.use(favicon(__dirname+'/public'+'/favicon.ico'))
 
 app.get('/',(req,res) => {
     res.render('index')
