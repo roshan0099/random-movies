5db0421bd8f398ce1f28654f3643951256403f40
Roshan <67196874+roshan0099@users.noreply.github.com>
2021-01-14 01:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(express.static(__dirname+'/public'))
-app.use(favicon(__dirname+'/public'+'/favicon.ico'))
+// app.use(favicon(__dirname+'/public'+'/favicon.ico'))
 
-app.get('*',(req,res) => {
+app.get('/',(req,res) => {
     res.render('index')
 })
 
